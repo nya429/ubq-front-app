4d9754f91e60645d77a54b5f0be656de76e719b3
greenchoicenow <song1991yue@gmail.com>
2018-01-24 13:34:03 -0500

    setup d3

--- src/app/map/map-demo/map-demo.component.ts
+++ src/app/map/map-demo/map-demo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-map-demo',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-demo.component.css']
 })
 export class MapDemoComponent implements OnInit {
-
+  @ViewChild('chart') private chartContainer: ElementRef;
+  @Input() private data: Array<any>;	
+  private margin: any = { top: 20, bottom: 20, left: 20, right: 20};		  
+  private chart: any;		  
+  private width: number;		  
+  private height: number;		  
+  private xScale: any;		  
+  private yScale: any;		  
+  private colors: any;		  
+  private xAxis: any;		  
+  private yAxis: any;
   constructor() { }
 
   ngOnInit() {
+    this.createMap();
   }
 
+  createMap() {
+    const element = this.chartContainer.nativeElement;
+    this.width = element.offsetWidth - this.margin.left - this.margin.right;		    
+    this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
+    const svg = d3.select(element).append('svg')		      
+              .attr('width', element.offsetWidth)		      
+              .attr('height', element.offsetHeight);
+
+    this.chart = svg.append('g')		      
+              .attr('class', 'bars')		      
+              .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);         
+    
+   const xDomain = this.data.map(d => d[0]);       
+    const yDomain = [0, d3.max(this.data, d => d[1])];         
+  }
 }
