dd5f97390acbad658b222c886469f29f2d068af0
greenchoicenow <song1991yue@gmail.com>
2018-02-09 19:46:07 -0500

    sss

--- src/app/map/tracker-detail/tracker-detail.component.ts
+++ src/app/map/tracker-detail/tracker-detail.component.ts
@@ -1,15 +1,31 @@
+import { Tracker } from './../../shared/tracker.model';
+import { Subscription } from 'rxjs/Subscription';
 import { Component, OnInit } from '@angular/core';
+
+import { MapService } from './../map.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-tracker-detail',
   templateUrl: './tracker-detail.component.html',
   styleUrls: ['./tracker-detail.component.css']
 })
-export class TrackerDetailComponent implements OnInit {
-
-  constructor() { }
+export class TrackerDetailComponent implements OnInit, OnDestroy {
+  tracker: Tracker;
+  private selectedTrackerSubscription: Subscription;
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
+    this.selectedTrackerSubscription = this.mapService.selectedTrackerIndex.subscribe(
+      (id: number) => {
+        this.tracker = this.mapService.getTracker(id);
+        console.log(this.tracker);
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.selectedTrackerSubscription.unsubscribe();
   }
 
 }
