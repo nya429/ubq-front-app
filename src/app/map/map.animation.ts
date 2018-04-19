f5062cee6c61c272d415fd3efc19ed37cbecece1
yue <yue@yues-MacBook-Pro.local>
2018-04-19 17:43:09 -0400

    add filter-zone

--- src/app/map/map.animation.ts
+++ src/app/map/map.animation.ts
@@ -34,4 +34,21 @@ export const  listItemFadeSlideStateTrigger =  trigger('listItemFadeSlideState',
     ])
 ]);
 
-
+export const hiddenItemStateTrigger = trigger('hiddenItemState', [
+    transition(':enter', [
+        style({
+            opacity: 0
+        }),
+        animate(100, style({
+            opacity: 1
+        }))
+    ]),
+    transition(':leave', [
+        style({
+            opacity: 1
+        }),
+        animate(100, style({
+            opacity: 0
+        }))
+    ]),
+]);
--- src/app/map/tracker-list/tracker-list.component.ts
+++ src/app/map/tracker-list/tracker-list.component.ts
@@ -1,13 +1,16 @@
-import { MapService } from './../map.service';
 import { Component, OnInit } from '@angular/core';
 
+import { MapService } from './../map.service';
+
 import { Tracker } from './../../shared/tracker.model';
-import { Output } from '@angular/core/src/metadata/directives';
+import { listItemSlideStateTrigger } from '../../participant-management/participant-list.animation';
+import { listItemFadeSlideStateTrigger } from '../map.animation';
 
 @Component({
   selector: 'app-tracker-list',
   templateUrl: './tracker-list.component.html',
-  styleUrls: ['./tracker-list.component.css']
+  styleUrls: ['./tracker-list.component.css'],
+  animations: [ listItemSlideStateTrigger, listItemFadeSlideStateTrigger ]
 })
 export class TrackerListComponent implements OnInit {
   trackers: Tracker[];
