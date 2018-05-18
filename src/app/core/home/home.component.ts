6a12600b3bfabe9c2103d9978e1e999750f72f8e
yue <yue@yues-MacBook-Pro.local>
2018-05-17 20:57:20 -0400

    side nav

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -12,9 +12,16 @@ import { LandpageService } from './../landpage.service';
 export class HomeComponent implements OnInit, OnDestroy {
   headerTriggered = false;
   serviceTriggered = false;
-  @ViewChild('intro') private introElement: ElementRef;
-  @ViewChild('scrollTrigger') private introTrigger: ElementRef;
-  @ViewChild('services') private servicesElement: ElementRef;
+  currentPos: number;
+
+  @ViewChild('intro') private introEl: ElementRef;
+  @ViewChild('services') private servicesEl: ElementRef;
+  @ViewChild('solutions') private solutionsEl: ElementRef;
+  @ViewChild('start') private startEl: ElementRef;
+  @ViewChild('team') private teamEl: ElementRef;
+  @ViewChild('contact') private contactEl: ElementRef;
+
+  @ViewChild('introTrigger') private introTrigger: ElementRef;
   private fragment: string;
 
   constructor(@Inject(DOCUMENT) private document: Document,
@@ -35,7 +42,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       window.scroll({top: (targetPos - 70),
       left: 0,
       behavior: 'smooth'});
-    }, 300);
+    }, 250);
     // el.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' });
   }
 
@@ -46,20 +53,42 @@ export class HomeComponent implements OnInit, OnDestroy {
     const windowScrollPos = this.document.documentElement.scrollTop;
 
     const introPos = this.introTrigger.nativeElement.offsetTop;
-    const introHeight = this.introElement.nativeElement.clientHeight;
-    const servicePos = this.servicesElement.nativeElement.offsetTop;
-    const serviceHeight = this.servicesElement.nativeElement.clientHeight;
+    const servicePos = this.servicesEl.nativeElement.offsetTop;
+    const solutionsPos = this.solutionsEl.nativeElement.offsetTop;
+    const startPos = this.startEl.nativeElement.offsetTop;
+    const teamPos = this.teamEl.nativeElement.offsetTop;
+    const contactPos = this.contactEl.nativeElement.offsetTop;
+
+    const introHeight = this.introEl.nativeElement.clientHeight;
+    const serviceHeight = this.servicesEl.nativeElement.clientHeight;
+
     // console.log(introPos, introHeight);
     if (windowScrollPos > (introPos - introHeight / 4)) {
       this.lpService.toggleHeaderOpacity(false);
+      this.headerTriggered = true;
     } else {
       this.lpService.toggleHeaderOpacity(true);
+      this.headerTriggered = false;
     }
-    console.log(windowScrollPos, servicePos, serviceHeight);
+    // console.log(windowScrollPos, servicePos, serviceHeight);
     if (!this.serviceTriggered && windowScrollPos > (serviceHeight / 2)) {
       this.serviceTriggered = true;
-      console.log(this.serviceTriggered);
     }
+
+    if (windowScrollPos < servicePos - 70) {
+      this.currentPos = 0;
+    } else if (windowScrollPos >= servicePos - 70 && windowScrollPos < solutionsPos - 70) {
+      this.currentPos = 1;
+    } else if (windowScrollPos >= solutionsPos - 70 && windowScrollPos < startPos - 70) {
+      this.currentPos = 2;
+    } else if (windowScrollPos >= startPos - 70 && windowScrollPos < teamPos - 70) {
+      this.currentPos = 3;
+    } else if (windowScrollPos >= teamPos - 70 && windowScrollPos < contactPos - 130) {
+      this.currentPos = 4;
+    } else {
+      this.currentPos = 5;
+    }
+    // console.log(this.currentPos);
   }
 
   fakeSend() {
